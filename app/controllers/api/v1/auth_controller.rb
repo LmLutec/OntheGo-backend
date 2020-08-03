8db9cca2b2815004eb403169dd82c23c5f900b12
Lauren McCoy <lm_lutec@outlook.com>
2020-08-02 20:30:05 -0400

    created authcontroller#create

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,2 +1,28 @@
 class Api::V1::AuthController < ApplicationController
+
+    def create
+        owner = Owner.find_by(username: login_params[:username])
+
+        if owner && owner.authenicate(login_params[:password])
+            token = encode_token({ owner_id: owner.id })
+        end 
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+private
+
+    def login_params
+        params.require(:owner).permit(:username, :password)
+    end 
 end
