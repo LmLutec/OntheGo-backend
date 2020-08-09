8c2abe15447f17f21e38ded041ae0a5635ae0516
Lauren McCoy <lm_lutec@outlook.com>
2020-08-09 13:56:29 -0400

    corrected placement of wrong code in schedule model. Created schedule controller. Now testing functionality of schedule#create

---
+++ app/controllers/api/v1/schedules_controller.rb
@@ -0,0 +1,19 @@
+class Api::V1::SchedulesController < ApplicationController
+
+    def create
+        schedule = Schedule.create(schedule_params)
+        render json: schedule
+    end     
+
+
+
+
+
+
+
+private
+
+    def schedule_params
+        params.require(:schedule).permit(:mon_start, :mon_end, :tues_start, :tues_end, :wed_start, :wed_end, :thurs_start, :thurs_end, :fri_start, :fri_end, :sat_start, :sat_end, :sun_start, :sun_end, :new_years_day, :thanksgiving_day, :christmas_day, :food_truck_id)
+    end 
+end
