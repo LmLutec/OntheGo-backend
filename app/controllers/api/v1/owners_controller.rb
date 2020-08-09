773d7e1dd8f137d5839731149312897945d033d9
Lauren McCoy <lm_lutec@outlook.com>
2020-08-08 20:01:03 -0400

    test push

--- app/controllers/api/v1/owners_controller.rb
+++ app/controllers/api/v1/owners_controller.rb
@@ -4,7 +4,9 @@ class Api::V1::OwnersController < ApplicationController
         owner = Owner.create(owner_params)
 
         if owner.valid? 
-
+            token = encode_token(owner_id: owner.id)
+            render json: { owner: owner, jwt: token}, status: :created 
+            byebug
         end 
     end 
 
