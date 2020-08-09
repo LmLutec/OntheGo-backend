1e43dc6f1cf19bec10d539d864eb925ba0de893f
Lauren McCoy <lm_lutec@outlook.com>
2020-08-09 13:10:33 -0400

    food truck #create works correctly. Moving on to creating schedule model and controller

--- app/controllers/api/v1/food_trucks_controller.rb
+++ app/controllers/api/v1/food_trucks_controller.rb
@@ -1,2 +1,22 @@
 class Api::V1::FoodTrucksController < ApplicationController
+
+    def create
+        food_truck = FoodTruck.create(food_truck_params)
+        render json: food_truck 
+    end 
+
+
+
+
+
+
+
+
+
+private
+
+    def food_truck_params
+        params.require(:food_truck).permit(:name, :food_type, :phone_number, :owner_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
     namespace :api do 
       namespace :v1 do 
         resources :owners
+        resources :food_trucks
         post '/login', to: 'auth#create'
-        # resources :auth
       end
     end
   
