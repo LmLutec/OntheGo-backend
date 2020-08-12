ce59a21ca3ba6c5d287a499178576a488bf44309
Lauren McCoy <lm_lutec@outlook.com>
2020-08-11 21:08:51 -0400

    work on how to call all food trucks by location. locations.food_trucks?

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::LocationsController < ApplicationController
     def create
         CS.states(:us).each do |s|
             if "#{s[0]}" == "#{locations_params["state"]}" 
-               
+            #    first letter of cities need to be capitalized, states are two characters long and capitalized (e.g. VA)
                 CS.cities("#{s[0]}").each do |city|
                     if "#{city}" == "#{locations_params["city"]}"
                         location = Location.create(locations_params)
