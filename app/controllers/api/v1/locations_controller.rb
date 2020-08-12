dfd3ec2ad1095e02ec0c20e46056ef82caa471a6
Lauren McCoy <lm_lutec@outlook.com>
2020-08-11 20:52:31 -0400

    location create works. Params are compared with city-state data to ensure the city and state are both valid

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -3,6 +3,7 @@ class Api::V1::LocationsController < ApplicationController
     def create
         CS.states(:us).each do |s|
             if "#{s[0]}" == "#{locations_params["state"]}" 
+               
                 CS.cities("#{s[0]}").each do |city|
                     if "#{city}" == "#{locations_params["city"]}"
                         location = Location.create(locations_params)
