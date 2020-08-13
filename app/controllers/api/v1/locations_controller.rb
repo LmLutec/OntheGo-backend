c5f92c0b78e212c9e5bb383d92d69ac26b3477ec
Lauren McCoy <lm_lutec@outlook.com>
2020-08-13 12:53:24 -0400

    created all location instances inside of seed file

--- app/controllers/api/v1/locations_controller.rb
+++ app/controllers/api/v1/locations_controller.rb
@@ -1,23 +1,30 @@
 class Api::V1::LocationsController < ApplicationController
 
     def create
-        find_location = Location.find_by(city: "#{locations_params["city"]}", state: "#{locations_params["state"]}")
+       location = Location.create(locations_params)
+        render json: location
+        # find_location = Location.find_by(city: "#{locations_params["city"]}", state: "#{locations_params["state"]}")
         
-        if !find_location
-            CS.states(:us).each do |s|
-                if "#{s[0]}" == "#{locations_params["state"]}" 
+        # if !find_location
+            # CS.states(:us).each do |s|
+            #     CS.cities("#{s[0]}").each do |city| puts city 
+            #         tester = Location.create(city: city, state: "#{s[0]}")
+            #     end
+            # end
+                # byebug
+                # if "#{s[0]}" == "#{locations_params["state"]}" 
                 #    first letter of cities need to be capitalized, states are two characters long and capitalized (e.g. VA)
-                    CS.cities("#{s[0]}").each do |city|
-                        if "#{city}" == "#{locations_params["city"]}"
-                            location = Location.create(locations_params)
-                            render json: location
-                        end
-                    end 
-                end
-            end
-        else
-            find_location
-        end 
+        #             CS.cities("#{s[0]}").each do |city|
+        #                 if "#{city}" == "#{locations_params["city"]}"
+        #                     location = Location.create(locations_params)
+        #                     render json: location
+        #                 end
+        #             end 
+        #         end
+        #     end
+        # else
+        #     find_location
+        # end 
     end 
 
 
