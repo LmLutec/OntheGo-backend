05b315d25b07e109c7f838351ec319bab8215ee1
Lauren McCoy <lm_lutec@outlook.com>
2020-10-29 11:24:32 -0400

    placed root inside v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
-  root :to => 'foodtrucks#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     namespace :api do 
       namespace :v1 do 
+        root :to => 'foodtrucks#search'
         resources :owners
         resources :foodtrucks
         resources :schedules
