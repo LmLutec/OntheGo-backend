d4b442ba1adc4f1959c07b091f69c940bf297b53
Lauren McCoy <lm_lutec@outlook.com>
2020-10-29 07:30:39 -0400

    root is foodtrucks#search

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   
-  root :to => 'auth#create'
+  root :to => 'foodtrucks#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     namespace :api do 
       namespace :v1 do 
-        resources :auth
         resources :owners
         resources :foodtrucks
         resources :schedules
